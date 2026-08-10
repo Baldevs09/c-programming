7756ec7c4fea98c6901292a19f3275f08f7b143f
Baldev singh <chaudharygunjan392@gmail.com>
2026-08-10 19:13:00 +0530

    prediction of day

--- problem.c
+++ problem.c
@@ -1,21 +1,8 @@
 #include<stdio.h>
-int main (){
-	int a,b;
-	printf("enter a number:");
-	scanf("%d",&a);
-	printf("enter a number:");
-	scanf("%d",&b);
-	if (a>b){
-		printf("a is largest%d",a);
-	}
-	else if  (b>a){
-		printf("b islargest %d",b);
-	}
-	else{
-		printf("both the numbers are eaual");
-	}
-
-
-
+int main(){
+	int issunday=0;
+	int issnowing=1;
+	printf("%d\n",issunday && issnowing);
 	return 0;
+
 }
