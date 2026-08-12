bfc08533918d77ae6924e4ee0fe3fa75f580673a
Baldev singh <chaudharygunjan392@gmail.com>
2026-08-12 17:43:10 +0530

    sum of two number

--- problem.c
+++ problem.c
@@ -1,5 +1,10 @@
 #include<stdio.h>
 int main(){
-	printf("hello world");
+	int a,b,sum;
+	printf("enter your number:");
+	scanf("%d",&a);
+	printf("enter your number:");
+	scanf("%d",&b);
+	sum=a+b;
+	printf("%d",sum);
 	return 0;
-}
