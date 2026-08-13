34494fafb7e16c35d231d5bb208af9b9b974591b
Baldev singh <chaudharygunjan392@gmail.com>
2026-08-13 18:52:13 +0530

    substract

--- problem.c
+++ problem.c
@@ -1,10 +1,21 @@
+// #include<stdio.h>
+// int main(){
+// 	int a,b,sum;
+// 	printf("enter your number:");
+// 	scanf("%d",&a);
+// 	printf("enter your number:");
+// 	scanf("%d",&b);
+// 	sum=a+b;
+// 	printf("%d",sum);
+// 	return 0;
+
 #include<stdio.h>
-int main(){
-	int a,b,sum;
+int main (){
+	int a,b;
 	printf("enter your number:");
 	scanf("%d",&a);
-	printf("enter your number:");
+	printf("enter your number");
 	scanf("%d",&b);
-	sum=a+b;
-	printf("%d",sum);
+	printf("sum of %d",a-b);
 	return 0;
+}
