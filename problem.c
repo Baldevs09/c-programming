9e5f67bfcd4bf1b3266cc2b94820bde8f9045833
Baldev singh <chaudharygunjan392@gmail.com>
2026-08-14 16:35:14 +0530

    swap two  number

--- problem.c
+++ problem.c
@@ -1,13 +1,13 @@
-// #include<stdio.h>
-// int main(){
-// 	int a,b,sum;
-// 	printf("enter your number:");
-// 	scanf("%d",&a);
-// 	printf("enter your number:");
-// 	scanf("%d",&b);
-// 	sum=a+b;
-// 	printf("%d",sum);
-// 	return 0;
+#include<stdio.h>
+int main(){
+	int a,b,sum;
+	printf("enter your number:");
+	scanf("%d",&a);
+	printf("enter your number:");
+	scanf("%d",&b);
+	sum=a+b;
+	printf("%d",sum);
+	return 0;
 
 #include<stdio.h>
 int main (){
@@ -17,5 +17,27 @@ int main (){
 	printf("enter your number");
 	scanf("%d",&b);
 	printf("sum of %d",a-b);
+
 	return 0;
 }
+
+#include <stdio.h>
+
+int main() {
+    int a, b, temp;
+
+    printf("Enter first number: ");
+    scanf("%d", &a);
+
+    printf("Enter second number: ");
+    scanf("%d", &b);
+
+    printf("Before swapping: a = %d, b = %d\n", a, b);
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    printf("After swapping: a = %d, b = %d\n", a, b);
+
+    return 0;}
