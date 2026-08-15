09b2e1e1211d151249fbc76632b539c02337ce30
Baldev singh <chaudharygunjan392@gmail.com>
2026-08-15 13:47:43 +0530

    which number is larger

--- problem.c
+++ problem.c
@@ -1,43 +1,64 @@
-#include<stdio.h>
-int main(){
-	int a,b,sum;
-	printf("enter your number:");
-	scanf("%d",&a);
-	printf("enter your number:");
-	scanf("%d",&b);
-	sum=a+b;
-	printf("%d",sum);
-	return 0;
+// #include<stdio.h>
+// int main(){
+// 	int a,b,sum;
+// 	printf("enter your number:");
+// 	scanf("%d",&a);
+// 	printf("enter your number:");
+// 	scanf("%d",&b);
+// 	sum=a+b;
+// 	printf("%d",sum);
+// 	return 0;
 
+// #include<stdio.h>
+// int main (){
+// 	int a,b;
+// 	printf("enter your number:");
+// 	scanf("%d",&a);
+// 	printf("enter your number");
+// 	scanf("%d",&b);
+// 	printf("sum of %d",a-b);
+
+// 	return 0;
+// }
+
+// #include <stdio.h>
+
+// int main() {
+//     int a, b, temp;
+
+//     printf("Enter first number: ");
+//     scanf("%d", &a);
+
+//     printf("Enter second number: ");
+//     scanf("%d", &b);
+
+//     printf("Before swapping: a = %d, b = %d\n", a, b);
+
+//     temp = a;
+//     a = b;
+//     b = temp;
+
+//     printf("After swapping: a = %d, b = %d\n", a, b);
+
+//     return 0;}
 #include<stdio.h>
 int main (){
 	int a,b;
-	printf("enter your number:");
+	printf("enter a number:");
 	scanf("%d",&a);
-	printf("enter your number");
+	printf("enter a number:");
 	scanf("%d",&b);
-	printf("sum of %d",a-b);
+	if (a>b){
+		printf("a is largest%d",a);
+	}
+	else if  (b>a){
+		printf("b islargest %d",b);
+	}
+	else{
+		printf("both the numbers are eaual");
+	}
+
+
 
 	return 0;
 }
-
-#include <stdio.h>
-
-int main() {
-    int a, b, temp;
-
-    printf("Enter first number: ");
-    scanf("%d", &a);
-
-    printf("Enter second number: ");
-    scanf("%d", &b);
-
-    printf("Before swapping: a = %d, b = %d\n", a, b);
-
-    temp = a;
-    a = b;
-    b = temp;
-
-    printf("After swapping: a = %d, b = %d\n", a, b);
-
-    return 0;}
