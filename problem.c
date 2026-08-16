e10557fb2c7788b2007faa99adfbd8cdb86bfe51
Baldev singh <chaudharygunjan392@gmail.com>
2026-08-16 07:47:18 +0530

    array

--- problem.c
+++ problem.c
@@ -41,24 +41,36 @@
 //     printf("After swapping: a = %d, b = %d\n", a, b);
 
 //     return 0;}
-#include<stdio.h>
-int main (){
-	int a,b;
-	printf("enter a number:");
-	scanf("%d",&a);
-	printf("enter a number:");
-	scanf("%d",&b);
-	if (a>b){
-		printf("a is largest%d",a);
-	}
-	else if  (b>a){
-		printf("b islargest %d",b);
-	}
-	else{
-		printf("both the numbers are eaual");
-	}
+// #include<stdio.h>
+// int main (){
+// 	int a,b;
+// 	printf("enter a number:");
+// 	scanf("%d",&a);
+// 	printf("enter a number:");
+// 	scanf("%d",&b);
+// 	if (a>b){
+// 		printf("a is largest%d",a);
+// 	}
+// 	else if  (b>a){
+// 		printf("b islargest %d",b);
+// 	}
+// 	else{
+// 		printf("both the numbers are eaual");
+// 	}
 
 
 
+// 	return 0;
+// }
+#include <stdlib.h>
+int main(){
+
+	int arr[]={45,65,85};
+//	if arr[i] % 2 even print the value otherwise dont print it
+
+for(int i=0;i<sizeof(arr)/sizeof(arr[0]);i++){
+	printf("%d ",i);
+
+}
 	return 0;
 }
