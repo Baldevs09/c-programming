5712dfd9b70b434a7a852ca2491a25329bc48d8a
Baldev singh <chaudharygunjan392@gmail.com>
2026-08-24 20:28:32 +0530

    even odd

--- problem.c
+++ problem.c
@@ -1,15 +1,31 @@
-//----------------------------------------------------------ARRAY-----------------------------------------------------------------------
-#include<stdio.h>
-int main(){
-	float price[3];
-	printf("enter your item price:");
-	scanf("%f",&price[0]);
-	printf("enter your item price:");
-	scanf("%f",&price[1]);
-	printf("enter your item price:");
-	scanf("%f",&price[2]);
-	printf("total price:%f",price[0]+(0.18*price[0]));
-	printf("total price:%f",price[1]+(0.18*price[1]));
-	printf("total price:%f",price[2]+(0.18*price[2]));
-	return 0;
+// //----------------------------------------------------------ARRAY-----------------------------------------------------------------------
+// #include<stdio.h>
+// int main(){
+// 	float price[3];
+// 	printf("enter your item price:");
+// 	scanf("%f",&price[0]);
+// 	printf("enter your item price:");
+// 	scanf("%f",&price[1]);
+// 	printf("enter your item price:");
+// 	scanf("%f",&price[2]);
+// 	printf("total price:%f",price[0]+(0.18*price[0]));
+// 	printf("total price:%f",price[1]+(0.18*price[1]));
+// 	printf("total price:%f",price[2]+(0.18*price[2]));
+// 	return 0;
+// }
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num % 2 == 0) {
+        printf("%d is Even\n", num);
+    } else {
+        printf("%d is Odd\n", num);
+    }
+
+    return 0;
 }
