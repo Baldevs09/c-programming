9d959998c9480a8c299ce81e2f10f24de9aa2783
Baldev singh <chaudharygunjan392@gmail.com>
2026-08-26 22:07:28 +0530

    prime number

--- problem.c
+++ problem.c
@@ -29,23 +29,49 @@
 
 //     return 0;
 // }
+// #include <stdio.h>
+
+// int main() {
+//     int a, b, c;
+
+//     printf("Enter three numbers: ");
+//     scanf("%d %d %d", &a, &b, &c);
+
+//     if (a >= b && a >= c) {
+//         printf("%d is the largest", a);
+//     }
+//     else if (b >= a && b >= c) {
+//         printf("%d is the largest", b);
+//     }
+//     else {
+//         printf("%d is the largest", c);
+//     }
+
+//     return 0;
+// }
 #include <stdio.h>
 
 int main() {
-    int a, b, c;
+    int n, i, isPrime = 1;
 
-    printf("Enter three numbers: ");
-    scanf("%d %d %d", &a, &b, &c);
+    printf("Enter a number: ");
+    scanf("%d", &n);
 
-    if (a >= b && a >= c) {
-        printf("%d is the largest", a);
+    if (n <= 1) {
+        isPrime = 0;
+    } else {
+        for (i = 2; i <= n / 2; i++) {
+            if (n % i == 0) {
+                isPrime = 0;
+                break;
+            }
+        }
     }
-    else if (b >= a && b >= c) {
-        printf("%d is the largest", b);
-    }
-    else {
-        printf("%d is the largest", c);
-    }
+
+    if (isPrime)
+        printf("%d is a prime number", n);
+    else
+        printf("%d is not a prime number", n);
 
     return 0;
 }
