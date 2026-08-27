40b8df57fdeb67d6875291919c6f7886e9e00818
Baldev singh <chaudharygunjan392@gmail.com>
2026-08-27 20:26:51 +0530

    reverse a number

--- problem.c
+++ problem.c
@@ -48,30 +48,48 @@
 //     }
 
 //     return 0;
+// // }
+// #include <stdio.h>
+
+// int main() {
+//     int n, i, isPrime = 1;
+
+//     printf("Enter a number: ");
+//     scanf("%d", &n);
+
+//     if (n <= 1) {
+//         isPrime = 0;
+//     } else {
+//         for (i = 2; i <= n / 2; i++) {
+//             if (n % i == 0) {
+//                 isPrime = 0;
+//                 break;
+//             }
+//         }
+//     }
+
+//     if (isPrime)
+//         printf("%d is a prime number", n);
+//     else
+//         printf("%d is not a prime number", n);
+
+//     return 0;
 // }
 #include <stdio.h>
 
 int main() {
-    int n, i, isPrime = 1;
+    int n, reverse = 0, rem;
 
     printf("Enter a number: ");
     scanf("%d", &n);
 
-    if (n <= 1) {
-        isPrime = 0;
-    } else {
-        for (i = 2; i <= n / 2; i++) {
-            if (n % i == 0) {
-                isPrime = 0;
-                break;
-            }
-        }
+    while (n != 0) {
+        rem = n % 10;
+        reverse = reverse * 10 + rem;
+        n = n / 10;
     }
 
-    if (isPrime)
-        printf("%d is a prime number", n);
-    else
-        printf("%d is not a prime number", n);
+    printf("Reverse = %d", reverse);
 
     return 0;
 }
